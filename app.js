a252a3fc3241c8004733b1cd983d68ba459147c9
Ruslanbek Kydyrmyshev <ruslanbekkydyrmyshev@Ruslanbeks-MacBook-Pro.local>
2022-03-01 00:27:51 -0500

    1st repo

---
+++ app.js
@@ -0,0 +1,7 @@
+let children = 'N';
+let partner = 'Z';
+let geoLocation = 'Y';
+let jobTitle = 'X';
+
+
+console.log(" You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids ");
